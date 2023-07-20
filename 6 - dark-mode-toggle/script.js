1b2fed265fbed3a7f8ffae97c3d999df27cecfe7
Bsit Malik <basithamed246@gmail.com>
2023-07-21 02:33:33 +0530

    project complete!

--- 6 - dark-mode-toggle/script.js
+++ 6 - dark-mode-toggle/script.js
@@ -1,16 +1,19 @@
 const inputEl = document.querySelector(".input");
 const bodyEl = document.querySelector("body");
 
+
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
 
 updateBody();
 function updateBody(){
     if(inputEl.checked){
-        bodyEl.style.background = "black"
+        bodyEl.style.background = "black";
+        bodyEl.style.color = "white";
     }
     else{
-        bodyEl.style.background = "white"
+        bodyEl.style.background = "white";
+        bodyEl.style.color = "black";
     }
 }
 inputEl.addEventListener("input",()=>{
